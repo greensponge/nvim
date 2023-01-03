d3ddc06900f0d67259b97b8841d4e7e93d4e97b9
greensponge <greensponge@gmail.com>
2023-01-03 09:52:03 +0200

    add conjure

--- lua/greensponge/packer.lua
+++ lua/greensponge/packer.lua
@@ -1,8 +1,8 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim  
 -- Only required if you have packer configured as `opt` 
-vim.cmd [[packadd packer.nvim]]  
+vim.cmd [[packadd packer.nvim]]
 
-return require('packer').startup(function(use) 
+return require('packer').startup(function(use)
 -- Packer can manage itself
 use 'wbthomason/packer.nvim'
 
@@ -45,5 +45,7 @@ use {
 		{'rafamadriz/friendly-snippets'},
 	}
 }
+
+use 'Olical/conjure'
 
 end)
