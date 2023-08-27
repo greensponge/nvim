10e9f807a74e6cb5be2f3a72d7b8d7148a577ec9
greensponge <greensponge@gmail.com>
2023-08-27 21:35:39 +0300

    fix harpoon config

--- lua/core/plugins/harpoon.lua
+++ lua/core/plugins/harpoon.lua
@@ -1,14 +1,15 @@
-local mark = require("harpoon.mark")
-local ui = require("harpoon.ui")
-local map = vim.keymap.set
-
-map("n", "<leader>h", mark.add_file, {desc = "Harpoon a file for later navigation. Unique bindings per project."})
-map("n", "<C-e>", ui.toggle_quick_menu, {desc = "Shows the Harpoon quick menu."})
-map("n", "<C-h>", function() ui.nav_file(1) end)
-map("n", "<C-t>", function() ui.nav_file(2) end)
-map("n", "<C-v>", function() ui.nav_file(3) end)
-map("n", "<C-s>", function() ui.nav_file(4) end)
-
 return {
-    'ThePrimeagen/harpoon'
+    'ThePrimeagen/harpoon',
+    config = function()
+        local mark = require("harpoon.mark")
+        local ui = require("harpoon.ui")
+        local map = vim.keymap.set
+
+        map("n", "<leader>h", mark.add_file, { desc = "Harpoon a file for later navigation. Unique bindings per project." })
+        map("n", "<C-e>", ui.toggle_quick_menu, { desc = "Shows the Harpoon quick menu." })
+        map("n", "<C-h>", function() ui.nav_file(1) end)
+        map("n", "<C-t>", function() ui.nav_file(2) end)
+        map("n", "<C-v>", function() ui.nav_file(3) end)
+        map("n", "<C-s>", function() ui.nav_file(4) end)
+    end
 }
