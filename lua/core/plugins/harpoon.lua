1028f0efed48770332f60bb63a6df0b91af7e63b
greensponge <greensponge@gmail.com>
2023-10-17 10:19:12 +0300

    update harpoon bindings

--- lua/core/plugins/harpoon.lua
+++ lua/core/plugins/harpoon.lua
@@ -5,11 +5,12 @@ return {
         local ui = require("harpoon.ui")
         local map = vim.keymap.set
 
-        map("n", "<leader>h", mark.add_file, { desc = "Harpoon a file for later navigation. Unique bindings per project." })
+        map("n", "<leader>h", mark.add_file,
+            { desc = "Harpoon a file for later navigation. Unique bindings per project." })
         map("n", "<C-e>", ui.toggle_quick_menu, { desc = "Shows the Harpoon quick menu." })
-        map("n", "<C-h>", function() ui.nav_file(1) end)
-        map("n", "<C-t>", function() ui.nav_file(2) end)
-        map("n", "<C-v>", function() ui.nav_file(3) end)
+        map("n", "<C-t>", function() ui.nav_file(1) end)
+        map("n", "<C-h>", function() ui.nav_file(2) end)
+        map("n", "<C-b>", function() ui.nav_file(3) end)
         map("n", "<C-s>", function() ui.nav_file(4) end)
     end
 }
