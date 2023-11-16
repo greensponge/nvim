3fa5bd993d91bc71ad36c05fb3a677c0c2ddf00f
greensponge <greensponge@gmail.com>
2023-11-16 10:12:34 +0200

    update telescope config, add search recent

--- lua/core/plugins/telescope.lua
+++ lua/core/plugins/telescope.lua
@@ -21,6 +21,8 @@ return {
 	},
 	config = function()
 		local telescope = require('telescope')
+		local builtin = require('telescope.builtin')
+
 		-- [[ Configure Telescope ]]
 		-- See `:help telescope` and `:help telescope.setup()`
 		telescope.setup {
@@ -39,23 +41,24 @@ return {
 		telescope.load_extension("projects")
 
 		-- See `:help telescope.builtin`
-		map('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-		map('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+		map('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recently opened files' })
+		map('n', '<leader><space>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 		map('n', '<leader>/', function()
 			-- You can pass additional configuration to telescope to change theme, layout, etc.
-			require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+			builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
 				winblend = 10,
 				previewer = false,
 			})
 		end, { desc = '[/] Fuzzily search in current buffer' })
 
-		map('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
-		map('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-		map('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-		map('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-		map('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-		map('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-		map('n', '<leader>sp', require('telescope').extensions.projects.projects,
+		map('n', '<leader>gf', builtin.git_files, { desc = 'Search [G]it [F]iles' })
+		map('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+		map('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+		map('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+		map('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+		map('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+		map('n', '<leader>sp', telescope.extensions.projects.projects,
 			{ desc = '[S]earch [P]rojects' })
+		map('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]ecent' })
 	end
 }
