78f07fa9656f59caa0056252fb9921451d8f74c5
greensponge <greensponge@gmail.com>
2024-03-18 09:51:59 +0200

    change tokyonight theme to moonfly

--- lua/core/plugins/theme.lua
+++ lua/core/plugins/theme.lua
@@ -1,7 +1,7 @@
 return {
-  'folke/tokyonight.nvim',
+  "bluz71/vim-moonfly-colors",
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme 'tokyonight'
+    vim.cmd.colorscheme "moonfly"
   end,
 }
