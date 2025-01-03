4e58360d9113cf52102bef4a7dc1db54bc7b88ad
greensponge <greensponge@gmail.com>
2025-01-03 11:48:51 +0200

    update lualine to look better

--- lua/core/plugins/lualine.lua
+++ lua/core/plugins/lualine.lua
@@ -1,13 +1,22 @@
 return {
   -- Set lualine as statusline
   'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
   -- See `:help lualine.txt`
   opts = {
     options = {
-      icons_enabled = false,
-      theme = 'onedark',
-      component_separators = '|',
-      section_separators = '',
+      icons_enabled = true,
+      theme = 'auto',
+      component_separators = { left = '', right = '' },
+      section_separators = { left = '', right = '' },
+    },
+    sections = {
+      lualine_a = { 'mode' },
+      lualine_b = { 'branch', 'diff', 'diagnostics' },
+      lualine_c = { 'filename' },
+      lualine_x = { 'fileformat', 'filetype' },
+      lualine_y = { 'progress' },
+      lualine_z = { 'location' }
     },
   },
 }
