e07ca1cb52f924e8873528552816f63a8a6418a4
greensponge <greensponge@gmail.com>
2025-01-03 23:29:17 +0200

    delete plugin global-note

--- lua/core/plugins/global-note.lua
+++
@@ -1,7 +0,0 @@
-return {
-	"backdround/global-note.nvim",
-	config = function()
-		local global_note = require("global-note")
-		vim.keymap.set("n", "<leader>n", global_note.toggle_note, { desc = "Toggle global note" })
-	end
-}
