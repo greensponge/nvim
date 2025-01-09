8639445901f02f6b0ce40cd8901f0fb1014a53f7
greensponge <greensponge@gmail.com>
2025-01-09 09:51:39 +0200

    fix conjure plugin setup

--- lua/core/plugins/conjure.lua
+++ lua/core/plugins/conjure.lua
@@ -1,20 +1,16 @@
-local M = {
+return {
   "Olical/conjure",
   ft = { "clojure", "fennel", "lua" },
   lazy = true,
+  config = function()
+    require("conjure.main").main()
+    require("conjure.mapping")["on-filetype"]()
+    vim.g["conjure#log#wrap"] = true
+    vim.g["conjure#highlight#enabled"] = true
+    vim.g["conjure#log#hud#width"] = 0.52
+    vim.g["conjure#log#hud#width"] = 0.4
+    vim.g["conjure#log#botright"] = true
+    vim.g["conjure#jump_to_latest#enabled"] = true
+    vim.g["conjure#log#hud#border"] = "rounded"
+  end
 }
-
-M.config = function()
-  require("conjure.main").main()
-  require("conjure.mapping")["on-filetype"]()
-
-  vim.g["conjure#log#wrap"] = true
-  vim.g["conjure#highlight#enabled"] = true
-  vim.g["conjure#log#hud#width"] = 0.52
-  vim.g["conjure#log#hud#width"] = 0.4
-  vim.g["conjure#log#botright"] = true
-  vim.g["conjure#jump_to_latest#enabled"] = true
-  vim.g["conjure#log#hud#border"] = "rounded"
-end
-
-return M
