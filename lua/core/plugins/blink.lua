d5a79fb1f866189a642ffe635aeb50884dfdef0a
greensponge <greensponge@gmail.com>
2025-01-21 11:09:24 +0200

    update blink config

--- lua/core/plugins/blink.lua
+++ lua/core/plugins/blink.lua
@@ -10,9 +10,15 @@ return {
       ['<Up>'] = { 'select_prev', 'fallback' },
       ['<Down>'] = { 'select_next', 'fallback' },
       ['<Enter>'] = { 'accept', 'fallback' },
+      ['<C-v>'] = { 'show', 'show_documentation', 'hide_documentation' },
     },
     completion = {
-      trigger = { show_on_keyword = false }
+      trigger = { show_on_keyword = true },
+      menu = {
+        auto_show = function(ctx)
+          return ctx.mode ~= "cmdline" or not vim.tbl_contains({ ':', '/', '?' }, vim.fn.getcmdtype())
+        end,
+      },
     },
     appearance = {
       use_nvim_cmp_as_default = true,
