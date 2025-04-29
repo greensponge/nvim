87892f82a42c526285940ada2fec4e77adc87129
greensponge <greensponge@gmail.com>
2025-04-29 13:07:21 +0300

    rebind harpoon first file shortcut

--- lua/core/plugins/harpoon.lua
+++ lua/core/plugins/harpoon.lua
@@ -14,7 +14,7 @@ return {
         map("n", "<leader>h", mark.add_file,
             { desc = "Harpoon a file for later navigation. Unique bindings per project." })
         map("n", "<C-e>", ui.toggle_quick_menu, { desc = "Shows the Harpoon quick menu." })
-        map("n", "<C-t>", function() ui.nav_file(1) end)
+        map("n", "<C-y>", function() ui.nav_file(1) end)
         map("n", "<C-h>", function() ui.nav_file(2) end)
         map("n", "<C-b>", function() ui.nav_file(3) end)
         map("n", "<C-s>", function() ui.nav_file(4) end)
